8c40e355eda0cfd8639b0929c5f12f62af015818
chknwngs999 <ryanlee4761@gmail.com>
2021-07-29 14:14:36 -0700

    +/- comments, implemented letter input feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 from sys import exit
-from random import randint
-
-#height, width (0,0) = top left
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -20,6 +18,7 @@ doublejump = False
 player_pull = 0
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_keys = [pygame.key.key_code(letter) for letter in alphabet]
 numbers = "1234567890"
 
 timed = 0
@@ -29,8 +28,6 @@ currency = 0
 blades = 0
 counted = 0
 shifted = False
-#seek_pf = 0
-#speed_pf = 0
 
 background = pygame.image.load('sprites/ubw_background_sprite.jpg').convert()
 background = pygame.transform.scale(background, (800, 600))
@@ -66,7 +63,8 @@ def display_time():
 #every other frame?
 #SHIFT INCREASES OVER LEVELS
 def obstacle_movement(obstacle_list):
-  global blades, score, currency, level, timed, time_obstacle_counted, shifted
+  global blades, score, currency, level, timed, time_obstacle_counted, shifted, obstacle_rect_list, obstacle_chr_list
+  corresponding_chr = 0
   if obstacle_list:
     for obstacle_rect in obstacle_list:
       obstacle_rect.y += 3 + int(level/2)
@@ -84,9 +82,17 @@ def obstacle_movement(obstacle_list):
           blades += 1
           score += 1 + level
           currency += 1 + level
+          index = obstacle_rect_list.index(obstacle_rect)
+          obstacle_rect_list.remove(obstacle_rect)
+          obstacle_chr_list.remove(obstacle_chr_list[index])
+        
+      letter_surf = test_font.render(chr(obstacle_chr_list[corresponding_chr]).upper(), False, (64, 64, 64))
+      letter_rect = letter_surf.get_rect(bottomright = (obstacle_rect.x, obstacle_rect.y))
+      pygame.draw.rect(screen, "#c0e8ec", letter_rect)
       screen.blit(weapon_surf, obstacle_rect)
+      screen.blit(letter_surf, letter_rect)
+      corresponding_chr += 1
     shifted = not shifted
-    obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.y < 600]
     return obstacle_list
   return []
 
@@ -110,10 +116,11 @@ def player_animation():
 
 weapon_surf = pygame.image.load('sprites/weapons/axe.png')
 weapon_surf = pygame.transform.scale(weapon_surf, (50, 50)).convert_alpha()
-weapon_rect = weapon_surf.get_rect(midtop = (randint(0, 800), 0))
+weapon_rect = weapon_surf.get_rect(midtop = (random.randint(0, 800), 0))
 
 weapon_list = []
 obstacle_rect_list = []
+obstacle_chr_list = []
 
 player_walk_1 = pygame.transform.scale(pygame.image.load('sprites/emiyasprite.png').convert_alpha(), (30, 45))
 player_walk_2 = pygame.transform.scale(pygame.image.load('sprites/emiyasprite.png').convert_alpha(), (30, 45))
@@ -136,11 +143,6 @@ game_message_rect = game_message.get_rect(center = (400, 450))
 
 spawn_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(spawn_timer, 1000 - (level*30))
-#25 works
-
-#topright, midtop, topleft
-#right, left
-#bottomright, midbottom, bottomleft
 
 while True:
   for event in pygame.event.get():
@@ -183,16 +185,28 @@ while True:
                 jumpamount = 20
             player_pull += jumpamount
             doublejump = True
-    
+
+        elif event.key in obstacle_chr_list:
+          blades += 1
+          score += 1 + level
+          currency += 1 + level
+          index = obstacle_chr_list.index(event.key)
+          obstacle_chr_list.remove(event.key)
+          obstacle_rect_list.remove(obstacle_rect_list[index])
+
       elif event.type == spawn_timer:
-        if randint(0, 2):
-          obstacle_rect_list.append(weapon_surf.get_rect(midtop = (randint(0, 800), 0)))
+        x_loc = random.randint(0, 800)
+        if random.randint(0, 2):
+          obstacle_rect_list.append(weapon_surf.get_rect(midtop = (x_loc, 0)))
         else:
           #spawn other weapon skin? 2:38:00
           #save to a list of surfs?
           #need to generate random letter for 
-          obstacle_rect_list.append(weapon_surf.get_rect(midtop = (randint(0, 800), 0)))
+
+          obstacle_rect_list.append(weapon_surf.get_rect(midtop = (x_loc, 0)))
+        obstacle_chr_list.append(random.choice(alphabet_keys))
     else:
+      #reset variables
       if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
         game_active = True
         player_rect.centerx = 400
@@ -205,30 +219,11 @@ while True:
         counted = 0
         shifted = False
         obstacle_rect_list.clear()
-  """if event.type == pygame.MOUSEMOTION/MOUSEBUTTONUP/MOUSEBUTTONDOWN:
-      print(event.pos)"""
+        obstacle_chr_list.clear()
   
   if game_active:
     screen.blit(background, (0, 0))
     timed = display_time()
-    """weapon_rect.y += 3 + int(level/2)
-    if weapon_rect.y < 500:
-      if player_rect.centerx > weapon_rect.centerx:
-        weapon_rect.centerx += 1
-        weapon_rect.centerx += level
-      elif player_rect.centerx < weapon_rect.centerx:
-        weapon_rect.centerx -= 1
-        weapon_rect.centerx -= level
-    else:
-      weapon_rect.y += 2"""
-
-    """if weapon_rect.y >= 600:
-      weapon_rect.y = 0
-      weapon_rect.centerx = randint(0, 800)
-      blades += 1
-      score += 1 + level
-      currency += 1 + level"""
-    #screen.blit(weapon_surf, weapon_rect)
 
     obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
@@ -246,6 +241,7 @@ while True:
     game_active = collisions(player_rect, obstacle_rect_list)
 
   else:
+    #check if game is in menu or ended
     screen.fill('Yellow')
     screen.blit(player_stand, player_stand_rect)
     
